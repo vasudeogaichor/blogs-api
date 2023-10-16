5ef16c264638038e67deb3b7278716d5626ecbbb
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-10-16 22:30:34 +0530

    criteria parsing init

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
+async function verifyCrieria(query: any) {
+    // Verify if the query params satisfy the pre-defined types for list criteria
+}
+
 export async function validateRequest(
     req: Request,
     res: Response,
@@ -9,8 +13,15 @@ export async function validateRequest(
     // Example: parsing JSON data from the request body
     try {
         // TODO - add validation and parsing logic
+        console.log('req - ', req.query)
+        switch (req.url) {
+            case '/posts':
+                await verifyCrieria(req.query)
+                break;
+        }
         next();
     } catch (error) {
         res.status(400).json({ error: 'Invalid JSON data' });
     }
 };
+
