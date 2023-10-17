556bfc60b86215c0d35a4b8405643d8d5194331b
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-10-17 22:21:53 +0530

    extract api service from request url

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,7 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
+function extractUrlPath(requestUrl: string) {
+    const match = requestUrl.match(/^\/(\w+)/);
+    
+    if (match) {
+        return match[1];
+    } else {
+        return null;
+    }
+}
+
 async function verifyCrieria(query: any) {
     // Verify if the query params satisfy the pre-defined types for list criteria
+}
+
+async function getCriteriaFormat(path: string) {
+
 }
 
 export async function validateRequest(
@@ -13,11 +27,13 @@ export async function validateRequest(
     // Example: parsing JSON data from the request body
     try {
         // TODO - add validation and parsing logic
-        console.log('req - ', req.query)
-        switch (req.url) {
-            case '/posts':
-                await verifyCrieria(req.query)
-                break;
+        console.log('req - ', req)
+        console.log('req.url - ', req.url)
+        const apiService = extractUrlPath(req.url)
+        console.log('apiService - ', apiService)
+        switch (req.method) {
+            case 'GET':
+                // TODO - add validation and parsing logic
         }
         next();
     } catch (error) {
