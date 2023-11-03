5a9d301f19c95b7f2ddeb5bb202f60e831078cd9
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-11-03 23:46:24 +0530

    added payload parsing based on api resource

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,13 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import * as criterias from "../criteria";
+import * as resources from "../resources";
 import { Criteria } from "../types/criteria";
-import { validationResult, body, check } from 'express-validator';
-
-const bodyValidationRules = [
-  body('field1').notEmpty().isString(),
-  body('field2').isInt(),
-  // Add more validation rules as needed for your specific fields.
-];
+import { Resource } from "../types/resources";
 
 function extractUrlPath(requestUrl: string) {
   const match = requestUrl.match(/^\/(\w+)/);
@@ -15,6 +10,59 @@ function extractUrlPath(requestUrl: string) {
   if (match) {
     return match[1];
   }
+}
+
+function parseResource(apiService: string, query: { [key: string]: any }, res: any) {
+  const parsedResource: { [key: string]: any } = {};
+
+  const resourceFormatObjectName: string = `${apiService}Resource`;
+
+  const resource: Resource = (resources as any)[resourceFormatObjectName];
+  for (const key in query) {
+    if (key in resource) {
+      const expectedType = resource[key].type;
+      let value = query[key];
+      
+      switch (expectedType.name) {
+        case 'Number':
+          if (isNaN(value)) {
+            res.status(400).json({ error: `Resource type is incorrect: ${expectedType.name} | value: ${value}` });
+          }
+          parsedResource[key] = parseInt(value, 10);
+          break;
+
+        case 'String':
+          let parsedValue;
+          if (value !== null && (typeof value !== 'string' || value.constructor !== String)) {
+            if (['number', 'boolean'].includes(typeof value) || value instanceof Date) {
+              value = String(value);
+
+            } else {
+              res.status(400).json({ error: `Resource type is incorrect: ${expectedType.name} | value: ${value}` });
+            }
+          }
+
+          if (value && typeof value === 'string') {
+            parsedValue = value.trim();
+          }
+          parsedResource[key] = parsedValue;
+          break;
+
+        case 'Boolean':
+          return value
+
+        case 'Date':
+          return value
+
+        default:
+          res.status(400).json({ error: `Unsupported Resource type: ${expectedType.name}` });
+          break;
+      }
+    } else {
+      res.status(400).json({ error: `Invalid resource - ${key}` });
+    }
+  }
+  return parsedResource;
 }
 
 // Verify if the query params satisfy the pre-defined types for list criteria
@@ -68,8 +116,8 @@ function parseCrieria(apiService: string, query: { [key: string]: any }, res: an
     } else {
       res.status(400).json({ error: `Invalid criteria - ${key}` });
     }
-    return parsedQuery;
   }
+  return parsedQuery;
 }
 
 export async function validateRequest(
@@ -90,18 +138,10 @@ export async function validateRequest(
 
       case "POST":
         // Add body validation
-        await Promise.all(bodyValidationRules.map(validationRule => validationRule.run(req)));
-
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        // TODO: Add parsing logic for request body
-        // For example, you can access the validated fields using req.body
-        const field1 = req.body.field1;
-        const field2 = req.body.field2;
+        const parsedResource: { [key: string]: any } = parseResource(apiService!, req.body, res);
+        req.body = parsedResource;
         break;
+
       case "PUT":
         break;
       case "DELETE":
