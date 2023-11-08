dc572b2c1d37bf2a61abf0c0d24436f76a24c695
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-11-08 23:37:37 +0530

    allowed a criteria to be an array also

--- src/criteria/postsCriteria.ts
+++ src/criteria/postsCriteria.ts
@@ -1,14 +1,16 @@
 import { Criteria } from "../types/criteria";
 
 const postsCriteria: Criteria = {
-    id: {
-        type: Number,
-        description: 'Id of the post'
-    },
+    id: [
+        {
+            type: Number,
+            description: "Id of the post",
+        },
+    ],
     query: {
         type: String,
-        description: 'Match query for title and description'
-    }
+        description: "Match query for title and description",
+    },
 };
 
 export default postsCriteria;
--- src/types/criteria.ts
+++ src/types/criteria.ts
@@ -2,5 +2,5 @@ export type Criteria = {
     [key: string]: {
       type: any; // Change 'any' to the appropriate type
       description: string;
-    };
+    } | Array<{ type: any; description: string }>;
   };
