904b679065d361292c47d6c09477e26fc4d07cf4
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-11-09 00:34:23 +0530

    change criteria interface

--- src/types/criteria.ts
+++ src/types/criteria.ts
@@ -1,6 +1,3 @@
 export type Criteria = {
-    [key: string]: {
-      type: any; // Change 'any' to the appropriate type
-      description: string;
-    } | Array<{ type: any; description: string }>;
+    [key: string]: { type: any; description: string } | { type: any; description: string }[];
   };
