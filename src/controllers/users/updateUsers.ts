782b41038e232e44d71d5fa66cbee5ebb31aefb1
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-27 00:34:51 +0530

    fix update user api

--- src/controllers/users/updateUsers.ts
+++ src/controllers/users/updateUsers.ts
@@ -3,41 +3,54 @@ import * as bcrypt from "bcrypt";
 import { Transaction } from "sequelize";
 import db from "../../database/connection";
 import { validateUserData } from "./createUsers";
-const saltRounds = 10
+import { User } from "../../interfaces";
+
+const saltRounds = 10;
 
 export default async function updateUsers(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-    let transaction: Transaction = await db.sequelize.transaction();
+  let transaction: Transaction = await db.sequelize.transaction();
 
   try {
     const { username, email, firstName, lastName, password } = req.body;
-    const { id } = req.params
+    const { id } = req.params;
 
     validateUserData(password, email, res);
 
-    const hashedPassword: string = await bcrypt.hash(password, saltRounds);
+    const dataToUpdate: User = {};
+
+    if (username) {
+      dataToUpdate.username = username;
+    }
+
+    if (firstName) {
+      dataToUpdate.first_name = firstName;
+    }
+
+    if (lastName) {
+      dataToUpdate.last_name = lastName;
+    }
+
+    if (password) {
+      dataToUpdate.hashed_password = await bcrypt.hash(password, saltRounds);
+    }
 
     const existingUser = await db.users.findByPk(id);
     if (!existingUser) {
-        return res.status(404).json({error: `Unable to find user with id: ${id}`})
+      return res
+        .status(404)
+        .json({ error: `Unable to find user with id: ${id}` });
     }
 
-    await existingUser.update({
-        username: username,
-        email: email,
-        first_name: firstName,
-        last_name: lastName,
-        hashed_password: hashedPassword
-    }, transaction)
+    await existingUser.update(dataToUpdate, transaction);
 
-    const result = {...existingUser.get(), hashed_password: undefined}
-    
-    await transaction.commit()
-    res.status(201).json({message: result})
+    const result = { ...existingUser.get(), hashed_password: undefined };
 
+    await transaction.commit();
+    res.status(201).json({ message: result });
   } catch (err) {
     console.log("Error - ", err);
     res.status(500).json({ error: `${err}` });
