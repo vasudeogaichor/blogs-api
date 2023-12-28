224cf2393089197f2f57beafde92a96f00e21e81
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-28 23:32:35 +0530

    added user delete api

--- src/controllers/users/deleteUsers.ts
+++ src/controllers/users/deleteUsers.ts
@@ -1,7 +1,35 @@
 import { Request, Response, NextFunction } from "express";
+import db from "../../database/connection";
 
-export default async function deleteUsers(req: Request,
-    res: Response,
-    next: NextFunction) {
+export default async function deleteUsers(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const id: Number = parseInt(req.params.id);
 
+    const user: any = await db.users.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({
+        error: `User with id ${id} does not exist`,
+      });
+    }
+
+    const deletedRecord: any = await user.destroy();
+
+    if (!deletedRecord.deleted_at) {
+      return res.status(400).json({
+        error: `Error deleting user with id ${id}`,
+      });
+    }
+
+    return res.status(204).json({
+      message: "User deleted successfully",
+    });
+  } catch (err) {
+    console.log("Error - ", err);
+    res.status(500).json({ error: `${err}` });
+  }
 }
